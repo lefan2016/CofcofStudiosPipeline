087b7487ff55f34d0b28f40d9d4686153e9874d9
Pablo Emmanuel De Leo <pabloemmanueldeleo@gmail.com>
2017-04-17 19:45:55 -0300

    new metho ui face

--- RIG/INTERFACE_VR_MICROSOFT_.py
+++ RIG/INTERFACE_VR_MICROSOFT_.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 import maya.cmds as cmds
-nameCharacter='BEARDGUY_GEO_'
-bearGuy=['BEARDGUY_BEARD_MSH',
+import maya.mel as mel
+nameCamera='BEARDGUY_FACE_UI_CAM'
+nameSpaceGeo='BEARDGUY_GEO_'
+nameSpaceRig='BEARDGUY_RIG_'
+
+
+CHAR1_GEO=['BEARDGUY_BEARD_MSH',
              'BEARDGUY_HEAD_MSH',
              'BEARDGUY_HAT_MSH',
              'BEARDGUY_BEARD_RING_MSH',
@@ -10,7 +15,7 @@ bearGuy=['BEARDGUY_BEARD_MSH',
              'BEARDGUY_R_EYE_MSH', 'BEARDGUY_EYEBROWS_MSH',
              'C_TEETH_UP_MSH','C_TEETH_DN_MESH', 'C_GUM_UP_MSH', 'C_GUM_DN_MSH', 'C_TONGUE_MSH']
 
-bearGuyCNT=[ 'C_BEARDGUY_HAT_CNT',
+CHAR1_CNT=[ 'C_BEARDGUY_HAT_CNT',
              'L_BEARDGUY_SHOULDERUP_CNT',
              'R_BEARDGUY_SHOULDERUP_CNT',
              'C_BEARDGUY_CAP0_CNT','C_BEARDGUY_CAP1_CNT',
@@ -22,7 +27,7 @@ bearGuyCNT=[ 'C_BEARDGUY_HAT_CNT',
              'C_BEARDGUY_BEARD1_CNT','C_BEARDGUY_BEARD2_CNT',
              'C_BEARDGUY_BEARD3_CNT']
 
-samurai=['BEARDSAMURAI_HELMET_MSH',
+CHAR2_GEO=['BEARDSAMURAI_HELMET_MSH',
              'BEARDSAMURAI_HEAD_MSH',
              'BEARDSAMURAI_NECK_MSH',
              'BEARDSAMURAI_MASK_MSH',
@@ -31,11 +36,11 @@ samurai=['BEARDSAMURAI_HELMET_MSH',
              'BEARDSAMURAI_SAMURAI_SKIRT_MSH',
              'BEARDGUY_SHOULDER_UP_MSH']
 
-samuraiCNTS=['L_BEARDGUY_SHOULDERUP_CNT', 'R_BEARDGUY_SHOULDERUP_CNT',
+CHAR2_CNT=['L_BEARDGUY_SHOULDERUP_CNT', 'R_BEARDGUY_SHOULDERUP_CNT',
             'L_BEARDSAMURAI_SAMURAI_SKIRT1_CNT', 'L_BEARDSAMURAI_SAMURAI_SKIRT0_CNT', 'R_BEARDSAMURAI_SAMURAI_SKIRT1_CNT',
             'R_BEARDSAMURAI_SAMURAI_SKIRT0_CNT', 'C_SAMURAYGUY_LACE0_CNT', 'C_SAMURAYGUY_LACE1_CNT', 'C_SAMURAYGUY_LACE2_CNT', 'C_SAMURAYGUY_LACE3_CNT']
 
-mexican=['BEARDGUY_MEXICAN_HAT_MSH',
+CHAR3_GEO=['BEARDGUY_MEXICAN_HAT_MSH',
              'BEARDGUY_PONCHO_MSH',
              'BEARDGUY_HEAD_MSH',
              'BEARDGUY_BEARD_MSH',
@@ -43,7 +48,7 @@ mexican=['BEARDGUY_MEXICAN_HAT_MSH',
              'BEARDGUY_R_EYE_MSH', 'BEARDGUY_EYEBROWS_MSH',
              'BEARDGUY_BEARD_RING_MSH','C_TEETH_UP_MSH','C_TEETH_DN_MESH', 'C_GUM_UP_MSH', 'C_GUM_DN_MSH', 'C_TONGUE_MSH']
 
-mexicanCNT=['L_BEARDGUY_MEXICAN_HATBALL_A_CNT','L_BEARDGUY_MEXICAN_HATBALL_B_CNT',
+CHAR3_CNT=['L_BEARDGUY_MEXICAN_HATBALL_A_CNT','L_BEARDGUY_MEXICAN_HATBALL_B_CNT',
              'L_BEARDGUY_MEXICAN_HATBALL_C_CNT','L_BEARDGUY_MEXICAN_HATBALL_D_CNT',
              'L_BEARDGUY_MEXICAN_HATBALL_E_CNT','L_BEARDGUY_MEXICAN_HATBALL_F_CNT',
              'R_BEARDGUY_MEXICAN_HATBALL_A_CNT','R_BEARDGUY_MEXICAN_HATBALL_B_CNT',
@@ -53,28 +58,19 @@ mexicanCNT=['L_BEARDGUY_MEXICAN_HATBALL_A_CNT','L_BEARDGUY_MEXICAN_HATBALL_B_CNT
              'C_BEARDGUY_BEARD3_CNT','C_BEARDGUY_HAT_CNT','L_BEARDGUY_SHOULDERUP_CNT',
              'R_BEARDGUY_SHOULDERUP_CNT']
 
-#nameSpace = ''
-#sel = mc.ls(sl=True)[0]
-#if mc.referenceQuery( sel, isNodeReferenced=True ):
-#    nameSpace = sel.split(':')[0] + ':'
-#namespaces = cmds.namespaceInfo(lon=True)
-#current_namespace = cmds.namespaceInfo(currentNamespace=True)
-#references = cmds.ls(type="reference")
+
 
 def agregarnameSpace(lista=[],nameSpace=''):
     newList=[]
-    namespacesScene = cmds.namespaceInfo(lon=True)
-    if nameSpace in namespacesScene:
-        for obj in lista:
-            if nameSpace in obj:
-                cmds.warning('Ya existe el namespace '+ nameSpace + ' en ' + obj)
-            else:
-                obj=nameSpace+':'+str(obj)
-                newList.append(obj)
-        return newList
-    else:
-        cmds.warning('No existe en la escena un namespace llamado: ' + nameSpace)
-        return lista
+    for obj in lista:
+        if nameSpace in obj:
+            raise cmds.warning('Ya existe el namespace '+ nameSpace + ' en ' + obj)
+            newList.append(obj)
+        else:
+            obj=nameSpace+':'+str(obj)
+            newList.append(obj)
+    return newList
+
 
 
 def comprobarObj(lista=[]):
@@ -92,63 +88,69 @@ def onOffVisibility(listaAprender=[], listaApagar=[] ):
         if obj not in listaAprender:
             cmds.setAttr(obj+'.v',0)
 
-def dinamicasOnOffMexico(onOff=False):
-    list=cmds.ls('*_BEARDGUY_MEXICAN_HATBALL_*_HSYM',r=True)
-    for o in list:
-        print o
-        if onOff==False:
-            cmds.setAttr(str(o)+'.simulationMethod', 0)
-            return False
-        if onOff==True:
-            cmds.setAttr(str(o)+'.simulationMethod', 3)
-            return True
+
 
 #Agrega el namespace si lo necesita
-bearGuy=agregarnameSpace(bearGuy,nameCharacter)
-bearGuyCNT=agregarnameSpace(bearGuyCNT)
+CHAR1_GEO=agregarnameSpace(CHAR1_GEO,nameSpaceRig+':'+nameSpaceGeo)
+CHAR1_CNT=agregarnameSpace(CHAR1_CNT,nameSpaceRig)
+comprobarObj(CHAR1_GEO)
+comprobarObj(CHAR1_CNT)
 
-samurai=agregarnameSpace(samurai,nameCharacter)
-samuraiCNTS=agregarnameSpace(samuraiCNTS)
+CHAR2_GEO=agregarnameSpace(CHAR2_GEO,nameSpaceRig+':'+nameSpaceGeo)
+CHAR2_CNT=agregarnameSpace(CHAR2_CNT,nameSpaceRig)
+comprobarObj(CHAR2_GEO)
+comprobarObj(CHAR2_CNT)
 
-mexican=agregarnameSpace(mexican,nameCharacter)
-mexicanCNT=agregarnameSpace(mexicanCNT)
-
-#Check si existen
-comprobarObj(bearGuy)
-comprobarObj(bearGuyCNT)
-
-comprobarObj(mexican)
-comprobarObj(samurai)
+CHAR3_GEO=agregarnameSpace(CHAR3_GEO,nameSpaceRig+':'+nameSpaceGeo)
+CHAR3_CNT=agregarnameSpace(CHAR3_CNT,nameSpaceRig)
+comprobarObj(CHAR3_GEO)
+comprobarObj(CHAR3_CNT)
 
 #Prende bearGuy
-def bearGuyBtn(*args):
-    onOffVisibility(bearGuy,samurai)
-    onOffVisibility(bearGuy,mexican)
-    onOffVisibility(bearGuyCNT,mexicanCNT)
-    onOffVisibility(bearGuyCNT,samuraiCNTS)
+def char1(*args):
+    onOffVisibility(CHAR1_GEO,CHAR2_GEO)
+    onOffVisibility(CHAR1_GEO,CHAR3_GEO)
+    onOffVisibility(CHAR1_CNT,CHAR2_CNT)
+    onOffVisibility(CHAR1_CNT,CHAR3_CNT)
 
 #Prende samurai
-def samuraiBtn(*args):
-    onOffVisibility(samurai,bearGuy)
-    onOffVisibility(samurai,mexican)
-    onOffVisibility(samuraiCNTS,mexicanCNT)
-    onOffVisibility(samuraiCNTS,bearGuyCNT)
+def char2(*args):
+    onOffVisibility(CHAR2_GEO,CHAR1_GEO)
+    onOffVisibility(CHAR2_GEO,CHAR3_GEO)
+    onOffVisibility(CHAR2_CNT,CHAR1_CNT)
+    onOffVisibility(CHAR2_CNT,CHAR3_CNT)
 #Prende mexican
-def mexicanBtn(*args):
-    onOffVisibility(mexican,bearGuy)
-    onOffVisibility(mexican,samurai)
-    onOffVisibility(mexicanCNT,bearGuyCNT)
-    onOffVisibility(mexicanCNT,samuraiCNTS)
+def char3(*args):
+    onOffVisibility(CHAR3_GEO,CHAR1_GEO)
+    onOffVisibility(CHAR3_GEO,CHAR2_GEO)
+    onOffVisibility(CHAR3_CNT,CHAR1_CNT)
+    onOffVisibility(CHAR3_CNT,CHAR2_CNT)
 
-def win(nameChar=''):
+def dinamicasOnOffMexico(onOff=False):
+    list=cmds.ls(nameSpaceRig+':*_BEARDGUY_MEXICAN_HATBALL_*HSYM')
+    for o in list:
+        o=cmds.listRelatives(o)[0]
+        if onOff==False:
+            mel.eval("setAttr "+str(o)+".simulationMethod 0")
+            print 'Dinamicas mexicanas OFF'
+            return False
+        if onOff==True:
+            mel.eval("setAttr "+str(o)+".simulationMethod 3")
+            print 'Dinamicas mexicanas ON'
+            return True
+def dinOnOff():
+    global onOff
+    onOff=dinamicasOnOffMexico((onOff==False))
+
+def win(namespace='',cameraName=''):
 
     if cmds.window('facialUi', ex=True):
         cmds.deleteUI('facialUi')
-    win = cmds.window('facialUi', title='FACIAL UI - ' + nameChar, widthHeight=[400,700] )
+    win = cmds.window('facialUi', title='FACIAL UI - '+ cameraName, widthHeight=[400,700] )
     FacialGUI = cmds.formLayout()
     panel=cmds.modelPanel()
 
-    FacialPanel=cmds.modelPanel(panel,edit=True,camera=nameChar + 'BEARDGUY_FACE_UI_CAM')
+    FacialPanel=cmds.modelPanel(panel,edit=True,camera=namespace + ':' + cameraName)
     cmds.formLayout(FacialGUI, e=True,
                     attachForm=[
                         (FacialPanel, "top", 0),
@@ -157,12 +159,12 @@ def win(nameChar=''):
                         (FacialPanel, "right", 0)])
 
     column = cmds.rowLayout(numberOfColumns=4)
-    cmds.symbolButton("bearGuyBtn", command=bearGuyBtn,image = r"F:\EMPRESAS\2VEINTE\MICROSOFT_VR\ASSETS\CHARS\02_BEARDGUY\beardguy_btn.png", width = 100, height = 100, backgroundColor = [0.2, 0.2, 0.2])
-    cmds.symbolButton("samuraiBtn", command=samuraiBtn,image = r"F:\EMPRESAS\2VEINTE\MICROSOFT_VR\ASSETS\CHARS\02_BEARDGUY\samurai_btn.png", width = 100, height = 100, backgroundColor = [0.2, 0.2, 0.2])
-    cmds.symbolButton("mexicanBtn", command=mexicanBtn,image = r"F:\EMPRESAS\2VEINTE\MICROSOFT_VR\ASSETS\CHARS\02_BEARDGUY\mexican_btn.png", width = 100, height = 100, backgroundColor = [0.2, 0.2, 0.2])
+    cmds.symbolButton("bearGuyBtn", command=char1,image = "beardguy_btn.png", width = 100, height = 100, backgroundColor = [0.2, 0.2, 0.2])
+    cmds.symbolButton("samuraiBtn", command=char2,image = "samurai_btn.png", width = 100, height = 100, backgroundColor = [0.2, 0.2, 0.2])
+    cmds.symbolButton("mexicanBtn", command=char3,image = "mexican_btn.png", width = 100, height = 100, backgroundColor = [0.2, 0.2, 0.2])
     column = cmds.rowLayout(numberOfColumns=1)
-    cmds.checkBox("dinamicCheckBox", label = "Dinamic_Mexican_Hat", onCommand = "dinamicasOnOffMexico(True)", offCommand = "dinamicasOnOffMexico(False)", value = 0)
+    cmds.checkBox("dinamicCheckBox", label = "Dinamic_Mexican_Hat", onCommand = 'dinamicasOnOffMexico(True)', offCommand = "dinamicasOnOffMexico(False)", value = 0)
 
     cmds.showWindow(win)
 
-win()
+win(nameSpaceRig,nameCamera)
