03071a925994f5374418a121971e2d4656efef8a
gabo <ale.gabo@hotmail.com>
2017-10-10 14:01:22 -0300

    fixing imports

--- RIG/PICKER_2DFACES/FacialNodes2DProjection.py
+++ RIG/PICKER_2DFACES/FacialNodes2DProjection.py
@@ -2,9 +2,9 @@ def dirs_files_dic ( mypath , filterExtension ):
     '''
     Returns a dic { Dir : files in Dir }. A dictonary with subdirectories and files inside them from a directory input.
     Example:
-        dirs_files_dic('O:\\EMPRESAS\\RIG_FACE2D\\ScriptingGuideRig\\Maps','png')
+        dirs_files_dic('O:\EMPRESAS\RIG_FACE2D\ScriptingGuideRig\Maps','png')
+    
     '''
-    from glob import glob
     from os import listdir
     from os.path import isfile, join
     returnDic = {}
