e06565f1283d25b4e166541d63dddfba3e3c6f8f
gabo <ale.gabo@hotmail.com>
2017-10-11 01:29:11 -0300

    El avance del dia para crear los nodos para el rig facial. esta hecho: file, conectado al layeredtexture, layeredtexture conectado a el layeredtexture general del pje, el layeredtexture general conectado al shder del pje.

--- RIG/PICKER_2DFACES/FacialNodes2DProjection.py
+++ RIG/PICKER_2DFACES/FacialNodes2DProjection.py
@@ -18,3 +18,40 @@ def dirs_files_dic ( mypath , filterExtension ):
         onlyfiles = [f for f in listdir(subdir) if ( isfile(join(subdir, f)) and os.path.splitext(f)[1]=='.'+filterExtension)]
         returnDic[subdir]= onlyfiles
     return returnDic
+
+def folders2Rig (mypath):
+    '''
+    Create a network for 2D facial rig system from a folder input by user.
+    '''
+    #mypath     = 'O:\EMPRESAS\RIG_FACE2D\ScriptingGuideRig\Maps'
+    dirsFiles  = dirs_files_dic( mypath ,'png')
+    layerTex   = createNode ( 'layeredTexture' , n = 'Face_LTX' )
+    layerTexShader = createNode ( 'layeredTexture' , n = 'FaceBody_LTX' )
+    pjShader   = createNode ( 'aiStandard' , n = 'Character_SHD' )
+    connectAttr ( layerTex + '.outColor'  , layerTexShader + '.inputs[0].color' )
+    connectAttr ( layerTex + '.outAlpha'  , layerTexShader + '.inputs[0].alpha' )
+    connectAttr ( layerTexShader + '.outColor'  , pjShader + '.color' )
+    for k in dirsFiles.keys():
+        layer = k.split('\\')[-1]
+        projector  = createNode ( 'projection' , n = layer + '_PRJ'   )
+        imgSeq     = createNode ( 'file'       , n = layer + '_FILE'   )
+        setAttr ( imgSeq + '.useFrameExtension' , 1)
+        try:
+            setAttr ( imgSeq + '.fileTextureName' , k + '\\' + dirsFiles[k][0] )
+        except:
+            pass
+        #img -> projection
+        connectAttr ( imgSeq + '.outColor'     , projector + '.image' )
+        for channel in ('R','G','B'):
+            connectAttr ( imgSeq + '.outAlpha' , projector + '.transparency' + channel )  # conecto imagen al projection
+        # projection -> layeredTexture
+        counter=0
+        attrConnected=False
+        while not attrConnected:
+            try:
+                connectAttr ( projector + '.outColor'  , layerTex + '.inputs['+ str(counter)+'].color' )
+                connectAttr ( projector + '.outAlpha'  , layerTex + '.inputs['+ str(counter)+'].alpha' )
+                attrConnected = True
+            except:
+                counter = counter + 1
+                pass
