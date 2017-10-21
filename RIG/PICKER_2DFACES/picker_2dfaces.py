08bb3982b93627743a10f7ce693505c5fc3d932a
Pablo Emmanuel De Leo <pabloemmanueldeleo@gmail.com>
2017-10-20 21:15:41 -0300

    cambios en la intreface

--- RIG/PICKER_2DFACES/picker_2dfaces.py
+++ RIG/PICKER_2DFACES/picker_2dfaces.py
@@ -2,9 +2,10 @@ import maya.cmds as cmds
 from functools import partial
 import random
 import sys
+import re
 path=r'F:\Repositores\GitHub\CofcofStudiosPipeline\RIG\UTIL'
 path2=r'F:\Repositores\GitHub\CofcofStudiosPipeline\RIG\PICKER_2DFACES'
-if not path in sys.path:
+if not (path or path2) in sys.path:
 	sys.path.append(path)
 	sys.path.append(path2)
 try:
@@ -15,12 +16,15 @@ except (RuntimeError, TypeError, NameError,IOError):
 
 name='MILO'
 nameSpace=''
-
+cabezaControl='L_EYE_PUPILA_CNT'
 mypath = 'O:\EMPRESAS\RIG_FACE2D\PERSONAJES\MILO\FACES'
 directorios = UTILITIES.dirs_files_dic(mypath, 'png', 'proxy')
 print directorios
 
-def getFrame(val=000, attr='', ctr='C_head_01_CTRL'):
+def getFrame(val='', attr='', ctr='C_head_01_CTRL',*args):
+  	print 'Frame: '+ str(val)
+	print 'Control: ' + ctr
+	print 'NameAttr: ' + attr
 	# Con esta funcion pregunto si existe el control que le estoy pasando por
 	# argumento
 	if cmds.objExists(ctr):
@@ -29,7 +33,6 @@ def getFrame(val=000, attr='', ctr='C_head_01_CTRL'):
 		mods = cmds.getModifiers()
 		# pregunto si se preciono y es shift y agrego a la seleccion
 		if (mods & 1) > 0:
-			print attr
 			cmds.setAttr(ctr + '.' + str(attr), val)
 		# de lo contrario solo selecciono
 		else:
@@ -38,17 +41,45 @@ def getFrame(val=000, attr='', ctr='C_head_01_CTRL'):
 		cmds.warning('No existe el control %s o existen dos iguales o necesita un namespace.' % (ctr))
 
 # Definimos una interfas grafica para el usuario
-def botonesUI(directorios='', nameSpace='',sizeButtons=100,parents=''):
+def botonesUI(directorios='', nameSpace='',sizeButtons=100,parents='',controlAttributos='L_EYE_PUPILA_CNT'):
+    #Contengo todo en un solo scroll grande
+    scroll=cmds.scrollLayout( 'scrollLayout', childResizable=True,parent=parents)
+    #Creo una fila con 3 columnos grandes
+    rowGeneral=cmds.rowLayout( numberOfColumns=3, columnWidth3=(sizeButtons*5, sizeButtons*6, sizeButtons*5), adjustableColumn3=1, columnAlign=(1, 'right'), columnAttach=[(1, 'left', 0), (2, 'both', 0), (3, 'left', 0)] )
+    colRight=cmds.columnLayout(adjustableColumn=True,parent=rowGeneral)
+    colMid=cmds.columnLayout(adjustableColumn=True,parent=rowGeneral)
+    colLeft=cmds.columnLayout(adjustableColumn=True,parent=rowGeneral)
+
     # creo los botones recoriendo el diccionario que creamos
     for key in directorios:
-        # Creo una columna para los botones
-        #cmds.columnLayout(adjustableColumn=True)
-        frame=cmds.frameLayout(label=key.split('\\')[-1],labelAlign='center',collapsable=True,parent=parents) #
-        cmds.rowColumnLayout( numberOfColumns = 4,parent=frame )
-        r,g,b=random.uniform(0.0,1.0),random.uniform(0.0,1.0),random.uniform(0.0,1.0)
 
+        #Ordeno los frames dependiendo de la letra que contengan las carpetas
+        sideFace=key.split('\\')[-1]
+        if 'l_' in sideFace:
+            sideParent=colLeft
+        elif 'r_' in sideFace:
+            sideParent=colRight
+        else:
+            sideParent=colMid
+
+
+        # Creo una columna para los botones
+        cl1=cmds.columnLayout(adjustableColumn=True,parent=sideParent)
+
+        frameIn=cmds.frameLayout(label=sideFace,labelAlign='center',collapsable=True,parent=cl1)
+        cl2=cmds.columnLayout( bgc=[0.4,0.4,0.6],cal='center',columnOffset=['both', 0],parent=frameIn)
+        cmds.text(label='DisplayLayer',align='center',parent=cl2)
+        cmds.checkBox(label='sideFace',parent=cl2)
+
+        cmds.frameLayout(label='Expresiones',labelAlign='center',collapsable=True)
+        cmds.rowColumnLayout( numberOfRows=4,bgc=[0.2,0.2,0.2])
+        #Para diferenciar las carpeas o frames le pongo diferentes colores
+        r,g,b=random.uniform(0.0,1.0),random.uniform(0.0,1.0),random.uniform(0.0,1.0)
+        #creo por cada file un boton
         for ctrl in directorios[key]:
-            val=000
+            #valFrame=[s.zfill(2) for s in re.findall(r'\b\d+\b', img)]
+            val=[int(s) for s in re.findall(r'\b\d+\b', ctrl)][0]
+
             # Solo si existe algo escrito en la variable nameSpace y si es asi
             # le agrego el nameSpace al control.
             if nameSpace is not '':
@@ -56,10 +87,10 @@ def botonesUI(directorios='', nameSpace='',sizeButtons=100,parents=''):
             # Agrego el boton y la funcion, con el nombre del value del
             # diccionario
             cmds.symbolButton(ctrl, image=key +'\\'+ctrl, width=sizeButtons, height=sizeButtons, backgroundColor=[r,g,b] ,
-                              annotation='( SHIFT+CLICK suma seleccion. )', command=partial(getFrame, val, key, ctrl))
+                              annotation='( SHIFT+CLICK suma seleccion. )', command=partial(getFrame, val, sideFace, controlAttributos))
 
 
-def UI(charName, directorios={}, nameSpace='', sizeButtons=60):
+def UI(charName='MILO', directorios={}, nameSpace='', sizeButtons=60,controlAttributo='L_EYE_PUPILA_CNT'):
     # variable que contiene el nombre de dockControl
     WorkspaceName = '2DPICKER_UI_' + charName
     # Pregunto si existe la ventana workspaceControl y si existe la borro
@@ -68,17 +99,10 @@ def UI(charName, directorios={}, nameSpace='', sizeButtons=60):
     if cmds.workspaceControl(WorkspaceName, exists=True):
         cmds.deleteUI(WorkspaceName)
         print 'Se borro', WorkspaceName
-        # ejecuto funcion de interfas y la guardo en un dock
-        cmds.workspaceControl(WorkspaceName, initialHeight=500, floating=False,
-                              retain=False,  dtm=('right', 1))
-        botonesUI( directorios, nameSpace,sizeButtons,WorkspaceName)
+    # ejecuto funcion de interfas y la guardo en un dock
+    cmds.workspaceControl(WorkspaceName, initialHeight=500,initialWidth=500, floating=False,
+                          retain=False,  dtm=('right', 1))
+    botonesUI( directorios, nameSpace,sizeButtons,WorkspaceName,)
 
 # llamo a la funcion la cual ejecuta todo el resto.
-
-name='MILO'
-nameSpace=''
-
-mypath = 'O:\EMPRESAS\RIG_FACE2D\PERSONAJES\MILO\FACES'
-directorios = UTILITIES.dirs_files_dic(mypath, 'png', 'proxy')
-print directorios
-UI(name, directorios, nameSpace,35)
+UI(name, directorios, nameSpace,60,cabezaControl)
