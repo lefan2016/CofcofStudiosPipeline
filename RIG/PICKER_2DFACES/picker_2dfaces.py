8635f2c0598a346d0fdc6d2b494d1b580686420c
Pablo Emmanuel De Leo <pabloemmanueldeleo@gmail.com>
2017-10-21 04:29:28 -0300

    funcion para hacer key y ahora lee bien las variables del control

--- RIG/PICKER_2DFACES/picker_2dfaces.py
+++ RIG/PICKER_2DFACES/picker_2dfaces.py
@@ -1,83 +1,112 @@
-import maya.cmds as cmds
-from functools import partial
+# @Date:   2017-10-10T11:13:42-03:00
+# @Last modified time: 2017-10-21T04:28:50-03:00
+
+
+
 import random
-import sys
 import re
-path=r'F:\Repositores\GitHub\CofcofStudiosPipeline\RIG\UTIL'
-path2=r'F:\Repositores\GitHub\CofcofStudiosPipeline\RIG\PICKER_2DFACES'
+import sys
+from functools import partial
+
+import maya.cmds as cmds
+
+path = r'F:\Repositores\GitHub\CofcofStudiosPipeline\RIG\UTIL'
+path2 = r'F:\Repositores\GitHub\CofcofStudiosPipeline\RIG\PICKER_2DFACES'
 if not (path or path2) in sys.path:
-	sys.path.append(path)
-	sys.path.append(path2)
+    sys.path.append(path)
+    sys.path.append(path2)
 try:
     import UTILITIES
     reload(UTILITIES)
-except (RuntimeError, TypeError, NameError,IOError):
+except (RuntimeError, TypeError, NameError, IOError):
     print 'NO SE PUDO IMPORTAR EL MODULO'
 
-name='MILO'
-nameSpace=''
-cabezaControl='L_EYE_PUPILA_CNT'
+name = 'MILO'
+nameSpace = ''
+cabezaControl = 'L_EYE_PUPILA_CNT'
 mypath = 'O:\EMPRESAS\RIG_FACE2D\PERSONAJES\MILO\FACES'
 directorios = UTILITIES.dirs_files_dic(mypath, 'png', 'proxy')
-print directorios
 
-def getFrame(val='', attr='', ctr='C_head_01_CTRL',*args):
-  	print 'Frame: '+ str(val)
-	print 'Control: ' + ctr
-	print 'NameAttr: ' + attr
-	# Con esta funcion pregunto si existe el control que le estoy pasando por
-	# argumento
-	if cmds.objExists(ctr):
-		currentVal = cmds.getAttr(ctr + '.' + str(attr))
-		# Devuelve el evento que preciono
-		mods = cmds.getModifiers()
-		# pregunto si se preciono y es shift y agrego a la seleccion
-		if (mods & 1) > 0:
-			cmds.setAttr(ctr + '.' + str(attr), val)
-		# de lo contrario solo selecciono
-		else:
-			cmds.setAttr(ctr + '.' + str(attr), currentVal)
-	else:
-		cmds.warning('No existe el control %s o existen dos iguales o necesita un namespace.' % (ctr))
+
+def setKeyNow(obj='L_EYE_PUPILA_CNT',attr='r_ojo'):
+    currentTimeX=cmds.currentTime( query=True )
+    cmds.setKeyframe( obj, attribute=attr, t=[currentTimeX,currentTimeX] )
+    cmds.keyTangent(obj, at=attr, itt='linear', ott='linear')
+    print 'KEY en :',obj,attr
+
+def getFrame(val=0, attr='r_ojo', ctr='L_EYE_PUPILA_CNT', *args):
+
+    # Con esta funcion pregunto si existe el control que le estoy pasando por
+    # argumento
+    if cmds.objExists(ctr + '.' + attr):
+        print ctr,attr, val
+        currentVal = cmds.getAttr(ctr + '.' + attr)
+        # Devuelve el evento que preciono
+        mods = cmds.getModifiers()
+        setKeyNow(ctr,attr)
+        # pregunto si se preciono y es shift y agrego a la seleccion
+        if (mods & 1) > 0:
+            print 'Estas apretando shift'
+
+            if 'l_' in attr:
+                attr='r_'+attr.split('l_')[1]
+                print 'Se seteo en el opuesto R tambien'
+                cmds.setAttr(ctr + '.' + attr, val)
+                setKeyNow(ctr,attr)
+            elif 'r_' in attr:
+                attr='l_'+attr.split('r_')[1]
+                print 'Se seteo en el opuesto L tambien'
+                cmds.setAttr(ctr + '.' + attr, val)
+                setKeyNow(ctr,attr)
+            else:
+                cmds.warning('No contiene el otro lado de la misma imagen nombrada L_ o R_')
+        # de lo contrario solo selecciono
+        else:
+            cmds.setAttr(ctr + '.' + attr, val)
+    else:
+        cmds.warning('No existe el atributo o variable '+attr+', en el control '+ctr+ ' o necesita de un namespace.')
 
 # Definimos una interfas grafica para el usuario
 def botonesUI(directorios='', nameSpace='',sizeButtons=100,parents='',controlAttributos='L_EYE_PUPILA_CNT'):
-    #Contengo todo en un solo scroll grande
-    scroll=cmds.scrollLayout( 'scrollLayout', childResizable=True,parent=parents)
-    #Creo una fila con 3 columnos grandes
-    rowGeneral=cmds.rowLayout( numberOfColumns=3, columnWidth3=(sizeButtons*5, sizeButtons*6, sizeButtons*5), adjustableColumn3=1, columnAlign=(1, 'right'), columnAttach=[(1, 'left', 0), (2, 'both', 0), (3, 'left', 0)] )
+
+    # Contengo todo en un solo scroll grande
+    scroll=cmds.scrollLayout( 'scrollLayout', parent=parents)
+    # Creo una fila con 3 columnos grandes
+    rowGeneral=cmds.rowLayout( numberOfColumns=3, columnWidth3=(sizeButtons*6, sizeButtons*6, sizeButtons*6), adjustableColumn3=1, columnAlign=(1, 'right'), columnAttach=[(1, 'left', 0), (2, 'both', 0), (3, 'left', 0)] )
     colRight=cmds.columnLayout(adjustableColumn=True,parent=rowGeneral)
     colMid=cmds.columnLayout(adjustableColumn=True,parent=rowGeneral)
     colLeft=cmds.columnLayout(adjustableColumn=True,parent=rowGeneral)
+    #color1=[0.24,0.31,0.24]
+    color1=random.uniform(0.0,1.0),random.uniform(0.0,1.0),random.uniform(0.0,1.0)
+    color2=[0.3,0.3,0.3]
 
     # creo los botones recoriendo el diccionario que creamos
     for key in directorios:
-
-        #Ordeno los frames dependiendo de la letra que contengan las carpetas
+        # Ordeno los frames dependiendo de la letra que contengan las carpetas
         sideFace=key.split('\\')[-1]
         if 'l_' in sideFace:
             sideParent=colLeft
+            colaps=False
         elif 'r_' in sideFace:
             sideParent=colRight
+            colaps=False
         else:
             sideParent=colMid
-
+            colaps=True
 
         # Creo una columna para los botones
         cl1=cmds.columnLayout(adjustableColumn=True,parent=sideParent)
 
-        frameIn=cmds.frameLayout(label=sideFace,labelAlign='center',collapsable=True,parent=cl1)
-        cl2=cmds.columnLayout( bgc=[0.4,0.4,0.6],cal='center',columnOffset=['both', 0],parent=frameIn)
-        cmds.text(label='DisplayLayer',align='center',parent=cl2)
-        cmds.checkBox(label='sideFace',parent=cl2)
-
-        cmds.frameLayout(label='Expresiones',labelAlign='center',collapsable=True)
-        cmds.rowColumnLayout( numberOfRows=4,bgc=[0.2,0.2,0.2])
-        #Para diferenciar las carpeas o frames le pongo diferentes colores
-        r,g,b=random.uniform(0.0,1.0),random.uniform(0.0,1.0),random.uniform(0.0,1.0)
-        #creo por cada file un boton
+        frameIn=cmds.frameLayout(label=sideFace.upper(),collapsable=True,parent=cl1,bgc=color1)
+        cl2=cmds.columnLayout( cal='right',cat=['both',0], columnOffset = ['both', 0] ,  adjustableColumn=True , parent = frameIn )
+        cmds.button(label='DisplayLayer')
+        expres=cmds.frameLayout(label='Expresiones',collapsable=True,collapse=colaps)
+        cmds.rowColumnLayout( numberOfRows=4,bgc=color2)
+        # Para diferenciar las carpeas o frames le pongo diferentes colores
+        #r,g,b=random.uniform(0.0,1.0),random.uniform(0.0,1.0),random.uniform(0.0,1.0)
+        # creo por cada file un boton
         for ctrl in directorios[key]:
-            #valFrame=[s.zfill(2) for s in re.findall(r'\b\d+\b', img)]
+            # valFrame=[s.zfill(2) for s in re.findall(r'\b\d+\b', img)]
             val=[int(s) for s in re.findall(r'\b\d+\b', ctrl)][0]
 
             # Solo si existe algo escrito en la variable nameSpace y si es asi
@@ -86,7 +115,7 @@ def botonesUI(directorios='', nameSpace='',sizeButtons=100,parents='',controlAtt
                 ctrl = nameSpace + ctrl
             # Agrego el boton y la funcion, con el nombre del value del
             # diccionario
-            cmds.symbolButton(ctrl, image=key +'\\'+ctrl, width=sizeButtons, height=sizeButtons, backgroundColor=[r,g,b] ,
+            cmds.symbolButton(ctrl, image=key +'\\'+ctrl, width=sizeButtons, height=sizeButtons, backgroundColor=color2 ,
                               annotation='( SHIFT+CLICK suma seleccion. )', command=partial(getFrame, val, sideFace, controlAttributos))
 
 
@@ -102,7 +131,7 @@ def UI(charName='MILO', directorios={}, nameSpace='', sizeButtons=60,controlAttr
     # ejecuto funcion de interfas y la guardo en un dock
     cmds.workspaceControl(WorkspaceName, initialHeight=500,initialWidth=500, floating=False,
                           retain=False,  dtm=('right', 1))
-    botonesUI( directorios, nameSpace,sizeButtons,WorkspaceName,)
+    botonesUI( directorios, nameSpace,sizeButtons,WorkspaceName,controlAttributo)
 
 # llamo a la funcion la cual ejecuta todo el resto.
-UI(name, directorios, nameSpace,60,cabezaControl)
+UI(name, directorios, nameSpace,30,cabezaControl)
